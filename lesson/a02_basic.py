af910ee5544e8328b06e226cce32df29b95b4884
Junghyo <tjwjdgy123@gamil.com>
2017-07-19 09:56:20 +0900

    lesson day2

---
+++ lesson/a02_basic.py
@@ -0,0 +1,11 @@
+
+import keyword
+print(keyword.kwlist)
+
+count = 100  # 정수 할당
+weight = 25.7  # 실수 할당
+name = "호날두"  # 문자열 할당
+print("count:", count)
+print("weight:", weight)
+print("name:", name)
+
