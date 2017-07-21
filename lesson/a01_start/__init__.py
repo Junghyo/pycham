26b76f1dc49d478bb83604be2011b6087cb41dad
Junghyo <tjwjdgy123@gmail.com>
2017-07-21 17:59:28 +0900

    lesson ~

---
+++ lesson/a01_start/__init__.py
@@ -0,0 +1,7 @@
+'''
+Created on 2017-07-21 17:51
+
+@ product name : PyCharm Community Edition
+
+@ author : yoda
+'''
---
+++ lesson/a05_class/__init__.py
@@ -0,0 +1,7 @@
+'''
+Created on 2017-07-21 17:53
+
+@ product name : PyCharm Community Edition
+
+@ author : yoda
+'''
