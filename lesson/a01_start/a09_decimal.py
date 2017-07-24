a4ec1fd9a36e6527007ae9ad61cfce70ae21d520
Junghyo <tjwjdgy123@gmail.com>
2017-07-24 11:31:55 +0900

    lesson decimal

--- lesson/a01_start/a09_decimal.py
+++ lesson/a01_start/a09_decimal.py
@@ -6,6 +6,6 @@ Created on 2017. 7. 19.
 from decimal import Decimal
 hap = Decimal(0.0)
 delta= Decimal('0.01')
-for i in range(0,999):
+for i in range(0, 999):
     hap += delta
 print("hap:", hap)
