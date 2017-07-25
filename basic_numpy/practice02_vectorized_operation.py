80524585b40fb3e8e8094011484b5bb7f9736380
Junghyo <tjwjdgy123@gmail.com>
2017-07-25 18:37:26 +0900

    practice ~vectorized operation (ing)

--- basic_numpy/practice02_vectorized_operation.py
+++ basic_numpy/practice02_vectorized_operation.py
@@ -31,6 +31,34 @@ print(z1[:10])
 
 a = np.array([1, 3, 3, 4, 7])
 b = np.arange(1, 6)
+c = np.arange(1, 6)
 print(a == b)    #[ True False  True  True False]
 
 print(a >= b)
+
+# 만약 배열 전체를 비교하고 싶다면 all을 사용한다
+print(np.all(b == c)) # True
+print(np.all(a > b))  # False
+
+# 지수 함수, 로그 함수 등의 수학 함수도 벡터화 연산을 지원한다.
+a = np.arange(5)
+print(np.exp(a)) # 지수 함수
+# print(np.log(a)) # 로그 함수
+print(10 ** a)
+# print(np.log10(a))
+
+print(np.log10(10))  # 1
+
+"""
+연습 문제 1
+
+벡터 x가 다음과 같을 때, 여러가지 수식을 사용하여 같은 크기의 벡터 y를 만든다.
+
+x = np.arange(10)
+"""
+x = np.arange(10)
+y = x
+print(y)
+z = np.linspace(1, 10, 10
+                )
+print(z)
