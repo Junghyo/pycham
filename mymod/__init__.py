fb3f955bdefe18ef4ea85ce3e467c08d0f09911a
Junghyo <tjwjdgy123@gmail.com>
2017-07-26 19:42:27 +0900

    my module ~ separation

---
+++ mymod/__init__.py
@@ -0,0 +1,7 @@
+'''
+Created on 2017-07-26 18:00
+
+@ product name : PyCharm Community Edition
+
+@ author : yoda
+'''
---
+++ mymod/print.py
@@ -0,0 +1,13 @@
+'''
+Created on 2017-07-26 18:01
+
+@ product name : PyCharm Community Edition
+
+@ author : yoda
+'''
+
+def div():
+    print("=" * 50)
+    print("next")
+    print("=" * 50)
+
