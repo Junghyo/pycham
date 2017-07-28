f72e4a1243cfd289e9f509f12fe67b54dee5e230
Junghyo <tjwjdgy123@gmail.com>
2017-07-28 17:45:28 +0900

    lesson ~mariaDB connect

--- lesson/a10_data_load/a05_dbconnect.py
+++ lesson/a10_data_load/a05_dbconnect.py
@@ -10,7 +10,23 @@ from mymod.print import *
 import numpy as np
 import pandas as pd
 import pymysql as pms
+"""
+MySQL Client
 
+비밀번호 입력 : 1234
+
+show databases;
+
+use test;
+
+show tables;
+char2(50) not null,' at line 3
+MariaDB [test]> create table good(
+    -> code int primary key,
+    -> name varchar(50) not null,
+    -> cnt int,
+    -> price int);
+"""
 con = pms.connect(host="localhost",
                   port=3306,
                   user="root",
