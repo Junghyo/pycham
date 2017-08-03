c16a557c3aad36d28a4270603f7c3f2ef9bc7c7b
Junghyo <tjwjdgy123@gmail.com>
2017-08-03 19:28:36 +0900

    practice ~probability model

---
+++ data_science/basic_probability/__init__.py
@@ -0,0 +1,14 @@
+'''
+Created on 2017-08-03 14:53
+
+@ product name : PyCharm Community Edition
+
+@ author : yoda
+'''
+
+from mymod.print import *
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pylab as plt
+
